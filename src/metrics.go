f64c57235c9d6baacedad2a81bb4da0aeaf7b2d9
Camden Cheek <camden.cheek@bluemedora.com>
2018-08-01 15:32:14 -0400

    First pass at metric collection. Untested

---
+++ src/metrics.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/newrelic/infra-integrations-sdk/data/metric"
+	"github.com/newrelic/infra-integrations-sdk/integration"
+)
+
+func metricsWorker(metricChan <-chan newRelicMetricSender, wg *sync.WaitGroup, i *integration.Integration) {
+	defer wg.Done()
+
+	tsMetricSets := make(map[string]*metric.Set)
+	instanceMetricSets := make(map[string]*metric.Set)
+
+	for {
+		metricSender, ok := <-metricChan
+		if !ok {
+			return
+		}
+
+		metric := metricSender.metric
+
+		if tsName, ok := metricSender.metadata["tablespace"]; ok {
+			ms := getOrCreateTablespaceMetricSet(tsName, tsMetricSets, i)
+			err := ms.SetMetric(metric.name, metric.value, metric.metricType)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
+		if instanceName, ok := metricSender.metadata["instanceID"]; ok {
+			ms := getOrCreateInstanceMetricSet(instanceName, instanceMetricSets, i)
+			err := ms.SetMetric(metric.name, metric.value, metric.metricType)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+}
+
+func getOrCreateInstanceMetricSet(instID string, m map[string]*metric.Set, i *integration.Integration) *metric.Set {
+	set, ok := m[instID]
+	if ok {
+		return set
+	}
+
+	e, _ := i.Entity(instID, "instance") //can't error if both name and namespace are defined
+	newSet := e.NewMetricSet("OracleDatabaseSample", metric.Attr("entityName", "instance:instanceID"+instID), metric.Attr("displayName", "instanceID"+instID))
+
+	m[instID] = newSet
+
+	return newSet
+}
+
+func getOrCreateTablespaceMetricSet(tablespaceName string, m map[string]*metric.Set, i *integration.Integration) *metric.Set {
+	set, ok := m[tablespaceName]
+	if ok {
+		return set
+	}
+
+	e, _ := i.Entity(tablespaceName, "tablespace") //can't error if both name and namespace are defined
+	newSet := e.NewMetricSet("OracleTablespaceSample", metric.Attr("entityName", "tablespace:"+tablespaceName), metric.Attr("displayName", tablespaceName))
+
+	m[tablespaceName] = newSet
+
+	return newSet
+}
--- src/oracledb.go
+++ src/oracledb.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"sync"
 
+	"github.com/jmoiron/sqlx"
 	sdkArgs "github.com/newrelic/infra-integrations-sdk/args"
-	"github.com/newrelic/infra-integrations-sdk/data/event"
-	"github.com/newrelic/infra-integrations-sdk/data/metric"
 	"github.com/newrelic/infra-integrations-sdk/integration"
 	_ "gopkg.in/goracle.v2"
 )
 
 type argumentList struct {
 	sdkArgs.DefaultArgumentList
-	SID      string `default:"" help:"The Oracle service name"`
-	Username string `default:"" help:"The OracleDB connection user name"`
-	Password string `default:"" help:"The OracleDB connection password"`
-	Hostname string `default:"127.0.0.1" help:"The OracleDB connection host name"`
-	Port     string `default:"1521" help:"The OracleDB connection port"`
+	SID             string `default:"" help:"The Oracle service name"`
+	Username        string `default:"" help:"The OracleDB connection user name"`
+	Password        string `default:"" help:"The OracleDB connection password"`
+	Hostname        string `default:"127.0.0.1" help:"The OracleDB connection host name"`
+	Port            string `default:"1521" help:"The OracleDB connection port"`
+	ExtendedMetrics bool   `default:"false" help:"Enable extended metrics"`
 }
 
 const (
@@ -34,47 +34,36 @@ func main() {
 	i, err := integration.New(integrationName, integrationVersion, integration.Args(&args))
 	panicOnErr(err)
 
-	db, err := sql.Open("goracle", "SYS:password@10.77.17.158/orcl")
-	if err != nil {
-		fmt.Println("Failed to create db object")
-	}
-
-	// Create Entity, entities name must be unique
-	e1, err := i.Entity("instance-1", "custom")
-	panicOnErr(err)
-
-	// Add Event
-	if args.All() || args.Events {
-		err = e1.AddEvent(event.New("restart", "status"))
-		panicOnErr(err)
-	}
-
-	// Add Inventory item
-	if args.All() || args.Inventory {
-		err = e1.SetInventoryItem("instance", "version", "3.0.1")
-		panicOnErr(err)
-	}
-
-	// Add Metric
 	if args.All() || args.Metrics {
-		m1 := e1.NewMetricSet("CustomSample")
-		err = m1.SetMetric("some-data", 1000, metric.GAUGE)
-		panicOnErr(err)
-	}
 
-	// Create another Entity
-	e2, err := i.Entity("instance-2", "custom")
-	panicOnErr(err)
+		db, err := sqlx.Open("goracle", "oracle://orcladmin:password@ora12-ref-1.bluemedora.localnet/DB12C?connectionClass=&poolIncrement=1&poolMaxSessions=8&poolMinSessions=0&sysdba=0&sysoper=0&standaloneConnection=0")
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	if args.All() || args.Inventory {
-		err = e2.SetInventoryItem("instance", "version", "3.0.4")
-		panicOnErr(err)
-	}
+		err = db.Ping()
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	if args.All() || args.Metrics {
-		m2 := e2.NewMetricSet("CustomSample")
-		err = m2.SetMetric("some-data", 2000, metric.GAUGE)
-		panicOnErr(err)
+		var wg sync.WaitGroup
+		var workerWg sync.WaitGroup
+		metricChan := make(chan newRelicMetricSender)
+		workerWg.Add(1)
+		go metricsWorker(metricChan, &workerWg, i)
+
+		wg.Add(4)
+		go oracleReadWriteMetrics.Collect(db, &wg, metricChan)
+		go oraclePgaMetrics.Collect(db, &wg, metricChan)
+		go oracleSysMetrics.Collect(db, &wg, metricChan)
+		go oracleTablespaceMetrics.Collect(db, &wg, metricChan)
+
+		go func() {
+			wg.Wait()
+			close(metricChan)
+		}()
+
+		workerWg.Wait()
 	}
 
 	panicOnErr(i.Publish())
