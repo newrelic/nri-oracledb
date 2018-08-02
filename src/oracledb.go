eab54fd8de78067088f7d20ed895d076e6370ee5
Camden Cheek <camden.cheek@bluemedora.com>
2018-08-02 16:04:32 -0400

    Created a working main

--- src/oracledb.go
+++ src/oracledb.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"sync"
 
 	"github.com/jmoiron/sqlx"
 	sdkArgs "github.com/newrelic/infra-integrations-sdk/args"
 	"github.com/newrelic/infra-integrations-sdk/integration"
-	_ "gopkg.in/goracle.v2"
+	"github.com/newrelic/infra-integrations-sdk/log"
+	goracle "gopkg.in/goracle.v2"
 )
 
 type argumentList struct {
@@ -28,7 +30,8 @@ const (
 )
 
 var (
-	args argumentList
+	args   argumentList
+	logger log.Logger
 )
 
 func main() {
@@ -36,23 +39,31 @@ func main() {
 	i, err := integration.New(integrationName, integrationVersion, integration.Args(&args))
 	panicOnErr(err)
 
+	logger = log.NewStdErr(args.Verbose)
+
+	sid := fmt.Sprintf("%s:%s/%s", args.Hostname, args.Port, args.ServiceName)
+	cp := goracle.ConnectionParams{
+		Username:    args.Username,
+		Password:    args.Password,
+		SID:         sid,
+		IsSysDBA:    args.IsSysDBA,
+		IsSysOper:   args.IsSysOper,
+		MaxSessions: 8,
+	}
+
+	// TODO when new version is released, change String to StringWithPassword
+	connString := strings.Replace(cp.String(), "SECRET", cp.Password, 1)
+	db, err := sqlx.Open("goracle", connString)
+	panicOnErr(err)
+
+	err = db.Ping()
+	panicOnErr(err)
+
+	var workerWg sync.WaitGroup
 	if args.All() || args.Metrics {
 
-		db, err := sqlx.Open("goracle", "oracle://orcladmin:password@ora12-ref-1.bluemedora.localnet/DB12C?connectionClass=&poolIncrement=1&poolMaxSessions=8&poolMinSessions=0&sysdba=0&sysoper=0&standaloneConnection=0")
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		err = db.Ping()
-		if err != nil {
-			fmt.Println(err)
-		}
-
 		var wg sync.WaitGroup
-		var workerWg sync.WaitGroup
 		metricChan := make(chan newRelicMetricSender)
-		workerWg.Add(1)
-		go metricsWorker(metricChan, &workerWg, i)
 
 		wg.Add(4)
 		go oracleReadWriteMetrics.Collect(db, &wg, metricChan)
@@ -65,8 +76,16 @@ func main() {
 			close(metricChan)
 		}()
 
-		workerWg.Wait()
+		workerWg.Add(1)
+		metricsWorker(metricChan, &workerWg, i)
 	}
+
+	if args.All() || args.Inventory {
+		workerWg.Add(1)
+		inventoryWorker(db, &workerWg, i)
+	}
+
+	workerWg.Wait()
 
 	panicOnErr(i.Publish())
 }
