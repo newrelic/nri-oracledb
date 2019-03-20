d1b75b6ba94f73f65a042ddf4b7913451f01f7bf
Camden Cheek <ccheek22@gmail.com>
2019-03-20 09:54:09 -0400

    Add log message if ORACLE_HOME is unset

--- src/oracledb.go
+++ src/oracledb.go
@@ -41,6 +41,11 @@ func main() {
 	i, err := integration.New(integrationName, integrationVersion, integration.Args(&args))
 	exitOnErr(err)
 
+  if oracleHome := os.Getenv("ORACLE_HOME"); oracleHome == "" {
+    log.Error("Required argument oracle_home is unset")
+    os.Exit(1)
+  }
+
 	// parse tablespace whitelist
 	err = parseTablespaceWhitelist()
 	exitOnErr(err)
