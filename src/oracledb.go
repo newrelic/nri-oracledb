cb1e5ef7b055cd48abc75328b95c08fafcf77786
Antonio Dias <antonio.r.dias@gmail.com>
2020-12-03 17:01:07 +0100

    feat: add option to disable connection pooling

--- src/oracledb.go
+++ src/oracledb.go
@@ -17,19 +17,20 @@ import (
 
 type argumentList struct {
 	sdkArgs.DefaultArgumentList
-	ServiceName         string `default:"" help:"The Oracle service name"`
-	Username            string `default:"" help:"The OracleDB connection user name"`
-	Password            string `default:"" help:"The OracleDB connection password"`
-	IsSysDBA            bool   `default:"false" help:"Is the user a SysDBA"`
-	IsSysOper           bool   `default:"false" help:"Is the user a SysOper"`
-	Hostname            string `default:"127.0.0.1" help:"The OracleDB connection host name"`
-	Tablespaces         string `default:"" help:"JSON Array of Tablespaces to collect. If empty will collect all tablespaces."`
-	Port                string `default:"1521" help:"The OracleDB connection port"`
-	ExtendedMetrics     bool   `default:"false" help:"Enable extended metrics"`
-	MaxOpenConnections  int    `default:"5" help:"Maximum number of connections opened by the integration"`
-	ConnectionString    string `default:"" help:"An advanced connection string. Takes precedence over host, port, and service name"`
-	CustomMetricsQuery  string `default:"" help:"A SQL query to collect custom metrics. Must have the columns metric_name, metric_type, and metric_value. Additional columns are added as attributes"`
-	CustomMetricsConfig string `default:"" help:"YAML configuration file with one or more custom SQL queries to collect"`
+	ServiceName           string `default:"" help:"The Oracle service name"`
+	Username              string `default:"" help:"The OracleDB connection user name"`
+	Password              string `default:"" help:"The OracleDB connection password"`
+	IsSysDBA              bool   `default:"false" help:"Is the user a SysDBA"`
+	IsSysOper             bool   `default:"false" help:"Is the user a SysOper"`
+	Hostname              string `default:"127.0.0.1" help:"The OracleDB connection host name"`
+	Tablespaces           string `default:"" help:"JSON Array of Tablespaces to collect. If empty will collect all tablespaces."`
+	Port                  string `default:"1521" help:"The OracleDB connection port"`
+	ExtendedMetrics       bool   `default:"false" help:"Enable extended metrics"`
+	MaxOpenConnections    int    `default:"5" help:"Maximum number of connections opened by the integration"`
+	ConnectionString      string `default:"" help:"An advanced connection string. Takes precedence over host, port, and service name"`
+	CustomMetricsQuery    string `default:"" help:"A SQL query to collect custom metrics. Must have the columns metric_name, metric_type, and metric_value. Additional columns are added as attributes"`
+	CustomMetricsConfig   string `default:"" help:"YAML configuration file with one or more custom SQL queries to collect"`
+	DisableConnectionPool bool   `default:"false" help:"Disables connection pooling. It may make make the integration run slower but may reduce issues with not being able to execute some queries due to ORA-24459 (failure to get new connection)"`
 }
 
 const (
@@ -94,14 +95,15 @@ func getConnectionString() string {
 	}
 
 	cp := goracle.ConnectionParams{
-		Username:      args.Username,
-		Password:      args.Password,
-		SID:           sid,
-		IsSysDBA:      args.IsSysDBA,
-		IsSysOper:     args.IsSysOper,
-		MinSessions:   0,
-		MaxSessions:   args.MaxOpenConnections,
-		PoolIncrement: 1,
+		Username:             args.Username,
+		Password:             args.Password,
+		SID:                  sid,
+		IsSysDBA:             args.IsSysDBA,
+		IsSysOper:            args.IsSysOper,
+		StandaloneConnection: args.DisableConnectionPool,
+		MinSessions:          0,
+		MaxSessions:          args.MaxOpenConnections,
+		PoolIncrement:        1,
 	}
 
 	return cp.StringWithPassword()
