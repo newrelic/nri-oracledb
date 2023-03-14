26fcad863a1c78840c7f7e81c666c635a2fa8155
Juan Manuel Perez <jperezflorez@newrelic.com>
2023-03-14 16:19:23 +0100

    untested code that I left uncommited

---
+++ test/terraform_modules/e2e_agent/remote-backend.tf
@@ -0,0 +1,10 @@
+data terraform_remote_state base_framework {
+  backend = "s3"
+
+  config = {
+    bucket         = "nr-coreint-terraform-tfstates"
+    dynamodb_table = "nr-coreint-terraform-locking"
+    key            = "base-framework/global-state-store.tfstate"
+    region         = "us-east-1"
+  }
+}
